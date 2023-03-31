7f1747895db799ade1f44263020e823b94354a1b
Michael Bittencourt <mchl.bittencourt@gmail.com>
2023-03-31 05:28:08 +0100

    feature: Improve exercise 1.a

--- atividade1a.py
+++ atividade1a.py
@@ -1,10 +1,27 @@
 from gpiozero import LED 
 from time import sleep
 
-led = LED(17)
+class LedHz:
 
-while True:
-	led.on()
-	sleep(1)
-	led.off()
-	sleep(1)
+    HZ_2 = 2
+
+    def __init__(self, ledPin, frequency):
+        self._led = LED(ledPin)
+        self._frequency = frequency
+
+    def blinkLed(self):
+        self._led.on()
+        sleep(1/(2*self._frequency))
+        self._led.off()
+        sleep(1/(2*self._frequency))
+
+    pass
+
+def main():
+
+    ledHz = LedHz(17, LedHz.HZ_2)
+
+    while True:
+        ledHz.blinkLed()
+
+main()
